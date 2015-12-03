9857d28a4f95410de0020dcbefe2247cc92f3c4a
Nick Robson <robson.nicholas1@gmail.com>
2015-12-03 18:10:58 +1100

    Day 1 (Bash)

--- Day 1/Part 2.sh
+++ Day 1/Part 2.sh
@@ -8,17 +8,13 @@ len=`expr $len + 0`
 instr=0
 floor=0
 for (( i = 0; i < $len; i++ )); do
-    prev=`expr $instr - 1`
     c="${input:$instr:1}"
-    oldfloor=$floor
-    #echo $c
     if [ "$c" = '(' ]; then
         floor=`expr $floor + 1`
     elif [ "$c" = ')' ]; then
         floor=`expr $floor - 1`
     fi
     instr=`expr $instr + 1`
-    #echo "$instr : $oldfloor => $floor"
     if [ $floor -lt 0 ]; then
         echo "Santa crossed into the basement at instruction $instr"
         exit
