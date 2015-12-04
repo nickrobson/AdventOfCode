1ddeb7b21789dc39051934f4200ab034cb6c6a51
Nick Robson <robson.nicholas1@gmail.com>
2015-12-04 18:29:34 +1100

    Day 4 (Groovy)

---
+++ Day 4/Part 1.groovy
@@ -0,0 +1,23 @@
+import java.security.MessageDigest;
+
+class Part1 {
+
+    static md5(s) {
+        MessageDigest digest = MessageDigest.getInstance("MD5")
+        digest.update(s.bytes)
+        new BigInteger(1, digest.digest()).toString(16).padLeft(32, '0')
+    }
+
+    static main(args) {
+        def input = "yzbqklnj"
+        def n = 1
+        while(true) {
+            if (md5(input + n).startsWith("00000")) {
+                println n
+                break;
+            }
+            n++
+        }
+    }
+
+}
