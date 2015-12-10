5c4f84ee9edc9801070f2c4e56924050fa991e92
Nick Robson <robson.nicholas1@gmail.com>
2015-12-10 21:15:33 +1100

    Day 10 (Go)
    This took for-fucking-ever to compute Part 2.
    .. 1+ hours

---
+++ Day 10/Part 2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "strconv"
+)
+
+func check(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
+
+func main() {
+    dat, err := ioutil.ReadFile("input")
+    fmt.Println(string(dat))
+    input := string(dat)
+    check(err)
+    fmt.Println("Input: " + input)
+
+    str := input
+    for x := 0; x < 50; x++ {
+        if x % 5 == 0 && x != 0 {
+            fmt.Print(" ")
+        }
+        fmt.Print(".")
+        count, n := 0, rune(-1)
+        newstr := ""
+        for _, m := range str {
+            if n != m {
+                if count > 0 {
+                    newstr += strconv.Itoa(count) + string(n)
+                }
+                n = m
+                count = 0
+            }
+            count++
+        }
+        newstr += strconv.Itoa(count) + string(n)
+        if x == 49 {
+            fmt.Println()
+        }
+        str = newstr
+    }
+    fmt.Println("Length: " + strconv.Itoa(len([]rune(str))))
+}
